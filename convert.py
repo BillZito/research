14cb1b752dd9a984a2d2956fe40d5888a234acdd
Bill Zito <bill.r.zito@gmail.com>
2017-03-22 12:40:47 -0700

    Convert images into videos

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
 '''
-run make file to convert each image in h5 to video clip
+run make_frame to convert each image in h5 to video clip
 
 '''
+import os
 import h5py
 import moviepy.editor as mpy
 dataset = '2016-04-21--14-48-08'
@@ -29,7 +30,7 @@ slice portion of camera based on i
 
 make sure that logs still match up with camera
 '''
-def cut_clip(ranges):
+def cut_clips(ranges):
   #TODO make string flexible to file name and current range
   for r in ranges:
     # print('r is', r)
@@ -50,14 +51,31 @@ def cut_clip(ranges):
 
       #TODO: increment by 1 to make end inclusive
     new_cam['X'] = cam['X'][int(start_cam):int(end_cam)]
-    print('complete one cam')
+    # print('complete one cam')
 
-
+'''
+THIS DOES NOT WORK
+goal: take 20+ clips in a dir, combines them, and saves
+line 72 not complete yet
+'''
+def combine_clips(dir):
+  all_files = os.listdir(dir)
+  combo_log = h5py.File('dataset/log2/combo_{}.h5'.format(str(dataset)), 'w')
+  print('allfiles_0', all_files[2])
+  first_file = h5py.File('dataset/log2/{}'.format(all_files[2]), 'r')
+  print('start loop', first_file)
+  for key in first_file:
+    print('key is', key)
+    combo_log[key] = []
+    for file_name in all_files[2:]:
+      file = h5py.File('dataset/log2/{}'.format(file_name), 'r')
+      combo_log[key] = combo_log[key][:] + file[key][:]
 
 
 
 if __name__ == '__main__':
-  cut_clip([[12, 14]])
+  # cut_clips([[16, 18], [18, 20], [20, 24], [26, 28]])
+  # combine_clips('dataset/log2/')
   # start = 81000
   # clip = mpy.VideoClip(make_frame, duration=10)
   # clip.write_videofile('test2.mp4', fps=100)
